4037474403c2e3a54918dd51bfa40d5d616ca429
Maximo Dominguez <mamodom@gmail.com>
2017-04-03 21:16:32 -0400

    Adding files

---
+++ SpecflowContextualAssertions/SpecFlowContextualAssertions.feature
@@ -0,0 +1,22 @@
+﻿Feature: SpecFlowContextualAssertions
+
+
+Scenario: All assertions pass
+	Given passing assertion
+	When passing assertion
+	Then passing assertion
+
+Scenario: Given assertion fails
+	Given failling assertion
+	When passing assertion
+	Then passing assertion
+
+Scenario: When assertion fails
+	Given passing assertion
+	When failling assertion
+	Then passing assertion
+
+Scenario: Then assertion fails
+	Given passing assertion
+	When passing assertion
+	Then failling assertion
---
+++ SpecflowContextualAssertions/Steps.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace SpecflowContextualAssertions
+{
+    [Binding]
+    public class Steps
+    {
+        [StepDefinition("passing assertion")]
+        public void PassingAssertion() =>
+            true.Should().BeTrue();
+
+        [StepDefinition("failling assertion")]
+        public void FaillingAssertion() =>
+            true.Should().BeFalse();
+
+        [AfterStep]
+        public void MarkSetupAssertionExceptionsAsInconclusive()
+        {
+            if (ScenarioContext.Current.TestError is AssertionException &&
+                ScenarioContext.Current.CurrentScenarioBlock == ScenarioBlock.Given)
+                Assert.Inconclusive(ScenarioContext.Current.TestError.Message);
+        }
+    }
+}
